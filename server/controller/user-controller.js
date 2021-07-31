b7f0c95ab56f3a1c2f36ba3465e29e319203497e
subhashkumar307 <subhashk644@gmail.com>
2021-07-31 12:08:39 +0530

    my last commit

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,3 +1,4 @@
+import { request, response } from 'express';
 import User from '../model/userSchema.js';
 
 
@@ -5,9 +6,9 @@ import User from '../model/userSchema.js';
 export const userSignup = async (request, response) => {
 
     try {
-        const exist= User.findOne({ username: request.body.username });
+        const exist= await User.findOne({ username: request.body.username });
         if(exist) {
-            return response.status(401).response('Username already exist');
+            return response.status(401).json('Username already exist');
         }
         const user = request.body;
         const newUser = new User(user);
@@ -16,5 +17,18 @@ export const userSignup = async (request, response) => {
         response.status(200).json('User is successfully registerd');
     } catch (error) {
         console.log('Error', error.message);
+    }
+}
+
+export const userLoginIn = async (request, response) => {
+    try{
+        let user = await User.findOne({ username: request.body.username, password: request.body.password });
+        if(user) {
+            return response.status(200).json(`${request.body.username} login successfull`);
+        } else {
+            return response.status(401).json('Invalid login');
+        }
+    } catch (error) {
+        console.log('Error:', error.message);
     }
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { userSignup } from '../controller/user-controller.js';
+import { userSignup, userLoginIn } from '../controller/user-controller.js';
 
 const router = express.Router();
 
 
 router.post('/signup', userSignup);
+router.post('/login', userLoginIn);
 
 
 export default router;
